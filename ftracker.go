ef1d02872c07a2d1ff295dd6889d8bf4e47e2a47
Maxim Buluchevskiy <maxim.buluchevskiy@mechta.kz>
2024-07-12 13:57:06 +0500

    third fixed commit :

--- ftracker.go
+++ ftracker.go
@@ -62,7 +62,7 @@ func ShowTrainingInfo(action int, trainingType string, duration, weight, height 
 		speed := swimmingMeanSpeed(lengthPool, countPool, duration)                // вызовите здесь необходимую функцию
 		calories := SwimmingSpentCalories(lengthPool, countPool, duration, weight) // вызовите здесь необходимую функцию
 		return fmt.Sprintf("Тип тренировки: %s\nДлительность: %.2f ч.\nДистанция: %.2f км.\nСкорость: %.2f км/ч\nСожгли калорий: %.2f\n", trainingType, duration, distance, speed, calories)
-	default
+	default:
 		return "неизвестный тип тренировки"
 	}
 }
